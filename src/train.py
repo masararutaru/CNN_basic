b93e47f692986b7f44affa18910f858eae98c1d0
masararutaru <198926341+masararutaru@users.noreply.github.com>
2025-12-04 03:33:43 +0900

    trainに書いてあった説明を削除,全てREADME.mdに移した。プロジェクトにおいてわからないことがあったらREADME.mdを参照

--- src/train.py
+++ src/train.py
@@ -1,10 +1,3 @@
-#docker compose exec app bash >
-# python -i src/train.py >
-# 書いたファイルを実行した状態から対話モードでdataの形やモデルについて確認できる
-#抜けたくなったらcontrol + dを押す(2回押したら対話モードから一気に通常のターミナルまで戻る)
-
-#学習中に Ctrl + c を1回押す
-# 普通ならそこでプログラムが強制終了して終わるが、-i オプションがついている場合は中断されたその瞬間の状態で対話モード（>>>）に入れる
 print("importing libraries...")
 import torch
 import torch.nn as nn
