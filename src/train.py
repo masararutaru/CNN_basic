29f9ebeeba8b012843de23d1980d62afb6cd5586
masararutaru <198926341+masararutaru@users.noreply.github.com>
2025-12-04 02:46:44 +0900

    dockercomposeの使い方説明を冒頭に追加,実行中に実行場所が分かるようにprint文を追加

--- src/train.py
+++ src/train.py
@@ -1,11 +1,23 @@
+#docker compose exec app bash >
+# python -i src/train.py >
+# 書いたファイルを実行した状態から対話モードでdataの形やモデルについて確認できる
+#抜けたくなったらcontrol + dを押す(2回押したら対話モードから一気に通常のターミナルまで戻る)
+
+#学習中に Ctrl + c を1回押す
+# 普通ならそこでプログラムが強制終了して終わるが、-i オプションがついている場合は中断されたその瞬間の状態で対話モード（>>>）に入れる
+print("importing libraries...")
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
 from dataset import get_cifar10_loaders
 
+print("setting device...")
 device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
-print(f"Using device : {decice}")
+print(f"Using device : {device}")
 
+print("loading dataset...")
 trainloader, testloader = get_cifar10_loaders(batch_size = 64)
+#画像の形状はtorch.Size([64, 3, 32, 32])
+
 
