6ef195de888e2ed225ab61bf4e068955f74308ca
masararutaru <198926341+masararutaru@users.noreply.github.com>
2025-12-05 02:26:57 +0900

    CNNモデルの実装

--- src/train.py
+++ src/train.py
@@ -11,6 +11,40 @@ print(f"Using device : {device}")
 
 print("loading dataset...")
 trainloader, testloader = get_cifar10_loaders(batch_size = 64)
-#画像の形状はtorch.Size([64, 3, 32, 32])
+#trainloaderの一枚の画像の形状は[ 3, 32, 32]
+
+
+n_output = 10
+n_hidden = 128
+
+print("model making...")
+class CNN(nn.Module):
+    def __init__(self, n_outputs, n_hidden):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 32, 3)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d((2,2))
+        self.flatten = nn.Flatten()
+        self.l1 = nn.Linear(32*14*14,n_hidden)
+        self.l2 = nn.Linear(n_hidden, n_outputs)
+
+        self.features = nn.Sequential(
+            self.conv1,
+            self.relu,
+            self.conv2,
+            self.relu,
+            self.maxpool)
+
+        self.classifier = nn.Sequential(
+            self.l1,
+            self.relu,
+            self.l2)
+
+    def forward(self, x):
+        x1 = self.features(x)
+        x2 = self.flatten(x1)
+        x3 = self.classifier(x2)
+        return x3
 
 
