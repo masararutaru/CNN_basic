51628b930455414218ac14310e0a1361b26bebf1
masararutaru <198926341+masararutaru@users.noreply.github.com>
2025-12-07 00:02:40 +0900

    モデル構成を変更。dropoutとconvを追加

--- src/train.py
+++ src/train.py
@@ -23,20 +23,41 @@ print("model making...")
 class CNN(nn.Module):
     def __init__(self, n_outputs, n_hidden):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3)
-        self.conv2 = nn.Conv2d(32, 32, 3)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3,padding=1)
+        self.conv2 = nn.Conv2d(32, 32, 3,padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3,padding=1)
+        self.conv4 = nn.Conv2d(64, 64, 3,padding=1)
+        self.conv5 = nn.Conv2d(64, 128, 3,padding=1)
+        self.conv6 = nn.Conv2d(128, 128, 3,padding=1)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d((2,2))
         self.flatten = nn.Flatten()
-        self.l1 = nn.Linear(32*14*14,n_hidden)
+        self.l1 = nn.Linear(2048,n_hidden)
         self.l2 = nn.Linear(n_hidden, n_outputs)
+        self.dropout1 = nn.Dropout(0.2)
+        self.dropout2 = nn.Dropout(0.3)
+        self.dropout3 = nn.Dropout(0.4)
 
         self.features = nn.Sequential(
             self.conv1,
             self.relu,
             self.conv2,
             self.relu,
-            self.maxpool)
+            self.maxpool,
+            self.dropout1,
+            self.conv3,
+            self.relu,
+            self.conv4,
+            self.relu,
+            self.maxpool,
+            self.dropout2,
+            self.conv5,
+            self.relu,
+            self.conv6,
+            self.relu,
+            self.maxpool,
+            self.dropout3,
+            )
 
         self.classifier = nn.Sequential(
             self.l1,
